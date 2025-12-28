8a2b0814451ed6fa9168a83b89fed7a9c1edf4d8
pk.sdw <sdwstst@gmail.com>
2025-12-28 23:10:20 +0300

    рабочая бета версия

---
+++ scripttest.js
@@ -0,0 +1,84 @@
+'use strict';
+ 
+// let myButton = document.getElementById('button');
+// const buttonValue =button.value;
+// console.log(buttonValue);
+// alert (buttonValue); 
+
+
+// function getValue(btn) {
+//   console.log(btn.value);
+//   let a = getValue(btn);
+//   alert( a);
+// }  
+// const a = document.getElementById('button');
+// const buttonValue = a.value;
+// console.log(buttonValue);
+
+// alert (butttonValye);
+
+// const elemOne = document.getElementById('button');
+// const a = elemOne.value;
+// const outputA = document.getElementById('outputA');
+// button.addEventListener('click', function() {
+// outputA.textContent = a;
+// });
+
+
+// let elemOne = document.getElementById('buttonSeven') || document.getElementById('buttonEight');
+// let outputA = document.getElementById('outputA');
+// button.addEventListener('click', function() {
+// let a = elemOne.value;  
+// outputA.textContent = a;
+// });
+
+// let click
+// let button = document.getElementById('buttonSeven');
+// button.addEventListener('click', function() {
+// click = console.log(button.value);  
+// });
+// alert(click)
+
+
+// let click
+// let button = document.getElementById('buttonSeven').addEventListener('click', function() {
+// console.log(this.value);  
+// });
+// alert(button)
+
+
+// function val() {
+//     let a = document.getElementById('buttonSeven').addEventListener('click', function () {
+//         let b = sev.value;
+//         alert (b)
+    
+//     });
+
+
+    // document.getElementById('buttonOne') ||
+    // document.getElementById('buttonTwo') ||
+    // document.getElementById('buttonThree') ||
+    // document.getElementById('buttonFor') ||
+    // document.getElementById('buttonFive') ||
+    // document.getElementById('buttonSix') ||
+    // document.getElementById('buttonSeven') ||
+    // document.getElementById('buttonEight') ||
+    // document.getElementById('buttonNine') ||
+    // document.getElementById('buttonZero');
+    
+//   alert(a.value);
+// }
+
+// function val() {
+//     // let a = document.getElementsByClassName('numberBtn')
+//     let a {
+        
+
+//     };
+// }
+
+
+const myBtn = document.getElementById("myBtn");
+myBtn.addEventListener("click", function(e) {
+    console.log("The button was cliced!");
+});
