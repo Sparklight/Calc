d57bdbca8db1cc6c17edd469912a14a9c8f8241b
pk.sdw <sdwstst@gmail.com>
2025-12-29 21:55:37 +0300

    дополнение функционала и изменение интерфейса

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 'use strict'
 
 const inputElement = document.getElementById('disp');
-
+let memory
 function btnClick (input) {
-  inputElement.value += input;  
+  inputElement.value += input; 
+  let example = inputElement.value;
+    let lastChar = example[example.length - 1];
+    let lastCharTwo = example[example.length - 2];
+  
+  if ((lastChar == '+' || lastChar == '-' || lastChar == '*' || lastChar == '/') & 
+      (lastCharTwo == '+' || lastCharTwo == '-' || lastCharTwo == '*' || lastCharTwo == '/')) {
+    let newExample = example.slice(0, -1);
+    inputElement.value = newExample;
+    console.log(newExample);
+  }
+
+  
 }
 
 function clearDisp () {
@@ -18,4 +30,17 @@ function calculate() {
         inputElement.value = "Error";
     }
 }
+function deleteOne() {
+    let example = inputElement.value;
+    let newExample = example.slice(0, -1);
+    inputElement.value = newExample;
+}
+function addInMemory() {
+    let addMemory = inputElement.value;
+    memory = addMemory.replace(/[^\w\s]|_/g, "");
+    
+}
 
+function useMemory() {
+    inputElement.value += memory
+}
